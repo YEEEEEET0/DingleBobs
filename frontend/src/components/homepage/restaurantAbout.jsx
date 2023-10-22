634e0ec8678117029859239561ecfbf172b5fbf3
Sliver <matas.jasevicius@gmail.com>
2023-10-22 21:58:48 +0300

    Restaurant info

--- frontend/src/components/homepage/restaurantAbout.jsx
+++ frontend/src/components/homepage/restaurantAbout.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
+
 const RestaurantAbout = () => {
   return (
-    <div className=''>
-        
+
+    <div className='restaurant-about'>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, necessitatibus recusandae labore quis exercitationem nisi culpa non saepe magnam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, neque et sed, expedita quidem nemo quae minus atque temporibus quis, molestiae inventore? Ipsa impedit laboriosam totam, ipsam voluptatibus ex. Nobis? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta molestias quasi temporibus alias aspernatur quas, mollitia vero minus distinctio ea quis maiores ipsum voluptatem blanditiis iusto architecto fugiat corporis explicabo! Aliquid dolore nisi quod, alias quos vitae animi beatae asperiores sint? </p>
+
+       <div className='details'>
+       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-car-front" viewBox="0 0 16 16">
+        <path d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2H6ZM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17 1.247 0 2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276Z"/>
+        <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679c.033.161.049.325.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.807.807 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.807.807 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155 1.806 0 4.037-.084 5.592-.155A1.479 1.479 0 0 0 15 9.611v-.413c0-.099-.01-.197-.03-.294l-.335-1.68a.807.807 0 0 0-.43-.563 1.807 1.807 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3H4.82Z"/>
+        </svg> Delivery time: N/A
+       </div>
+
+       <div className='details'>
+       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+        </svg> Alergens: N/A
+       </div>
     </div>
+    
+
+
   );
 }
 
--- frontend/src/components/homepage/restaurantDetail.jsx
+++ frontend/src/components/homepage/restaurantDetail.jsx
@@ -1,6 +1,8 @@
 // RestaurantDetail.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import RestaurantAbout from './restaurantAbout'
+
 
 const RestaurantDetail = () => {
   // Use useParams to get the restaurant name from the URL
@@ -31,18 +33,21 @@ const RestaurantDetail = () => {
   }
 
   return (
-    <div>
-      <div className='thumbnail'>
-        <h2>{restaurant.name}</h2>
+    <div className='all-info'>
+        <div className='thumbnail'>
+            <h2>{restaurant.name}</h2>
 
-        <img src={restaurant.imageurl} alt={restaurant.name}/>
+            <img src={restaurant.imageurl} alt={restaurant.name}/>
 
-        <p>Rating: {restaurant.rating} <svg fill="#FFFF00" xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="bi bi-star-fill" viewBox="0 0 16 16">
-                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.830-4.730-3.659-3.516c-.329-.314-.158-.888.283-.950l4.898-.696 2.187-4.328c.197-.390.730-.390.927 0l2.187 4.328 4.898.696c.441.062.612.636.283.950l-3.659 3.516.830 4.730c.078.443-.360.790-.746.592L8 13.187l-3.389 2.256z" />
-              </svg></p>
-    </div>
+            <p>Rating: {restaurant.rating} <svg fill="#FFFF00" xmlns="http://www.w3.org/2000/svg" width="16" height="16" className="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.830-4.730-3.659-3.516c-.329-.314-.158-.888.283-.950l4.898-.696 2.187-4.328c.197-.390.730-.390.927 0l2.187 4.328 4.898.696c.441.062.612.636.283.950l-3.659 3.516.830 4.730c.078.443-.360.790-.746.592L8 13.187l-3.389 2.256z" />
+                </svg></p>
+        </div>
       
-      {/* Add more details as needed */}
+      <RestaurantAbout/>
+
+      <div className='bottom-test'>test</div>
+      
     </div>
   );
 };
