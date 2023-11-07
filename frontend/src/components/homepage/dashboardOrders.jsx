29580eca5869d79180924c1eaa3379208fa5f47b
EcksV <domcejusk3@gmail.com>
2023-11-07 19:43:52 +0200

    a

---
+++ frontend/src/components/homepage/dashboardOrders.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef, useState } from 'react';
+import NavBlob from './navBlob';
+
+const DashboardOrders = () => {
+    return (
+        <div className='dashbody'>
+            <NavBlob />
+        
+        </div>
+    );
+}
+
+export default DashboardOrders;
