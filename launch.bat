fc37c1e75e65c9ec84ccfcc84949d3fcf7b95945
EcksV <domcejusk3@gmail.com>
2023-11-19 23:30:26 +0200

    improved launch

--- launch.bat
+++ launch.bat
@@ -1,3 +1,16 @@
-cd ./frontend
-npm run build
-start node ./backend/index.js
+@echo off
+
+if exist "compiled_frontend" (
+    echo The compiled_frontend folder exists.
+    cd frontend
+    npm run build
+    if %errorlevel% equ 0 (
+        echo Frontend build successful. Starting backend...
+        cd ..
+        start node backend\index.js
+    ) else (
+        echo Frontend build failed.
+    )
+) else (
+    echo The compiled_frontend folder does not exist.
+)
