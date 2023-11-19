0a1ccaaa6fb4d3c7edf24082c287d5beb46a1495
EcksV <domcejusk3@gmail.com>
2023-11-19 23:35:49 +0200

    new installer

--- install.bat
+++ install.bat
@@ -1,6 +1,24 @@
-@echo on
-start "" cmd /c "cd ./backend & npm i" 
-@echo installed backend packages
-start "" cmd /c "cd ./frontend & npm i" 
-@echo installed frontend packages
+@echo off
+
+REM Function to echo colored text
+:colorEcho
+echo off
+<nul set /p ".=%~2"
+findstr /v $ "%~2" nul
+echo.
+
+if "%~1" neq "" exit /b %~1
+
+REM Echoing with colors
+echo Installing backend packages...
+start "" cmd /c "cd ./backend & npm i"
+echo Installed backend packages
+echo.
+
+echo Installing frontend packages...
+start "" cmd /c "cd ./frontend & npm i"
+echo Installed frontend packages
+echo.
+
+pause
 exit
