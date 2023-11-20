83c9199934c66abba292dff1984c7c9d519a2078
Sliver <matas.jasevicius@gmail.com>
2023-11-20 17:57:23 +0200

    cart

--- frontend/src/components/homepage/cartListButton.jsx
+++ frontend/src/components/homepage/cartListButton.jsx
@@ -11,7 +11,13 @@ const cartListButton = () => {
         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+
+        <hr />
+
+        
+        <input id='cardName' type="text"/>
       </Dropdown.Menu>
+        
     </Dropdown>
   );
 }
