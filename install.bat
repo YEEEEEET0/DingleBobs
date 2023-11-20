967c34b8a63a2d1afb27fe6ff10cdc2e921a1615
EcksV <domcejusk3@gmail.com>
2023-11-20 17:12:02 +0200

    ok

--- install.bat
+++ install.bat
@@ -1,14 +1,17 @@
 @echo off
 
-echo Installing npm packages for backend...
-cd backend
-npm install
-cd ..
+REM Function to install packages in a directory
+:installPackages
+cd /d "%~dp0%~1"
+set "currentDir=%cd%"
+echo Installing %2 packages in directory: "%currentDir%"...
+start npm i >nul 2>&1
+echo %2 packages installation initiated in directory: "%currentDir%".
+echo.
 
-echo Installing npm packages for frontend...
-cd frontend
-npm install
-cd ..
+REM Install packages for backend and frontend
+call :installPackages "backend" Backend
+call :installPackages "frontend" Frontend
 
-echo Packages installation complete for both backend and frontend.
 pause
+exit
