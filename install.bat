f05c31f93ad5dcbd0ab9ec45073dd4281911fe73
EcksV <domcejusk3@gmail.com>
2023-11-20 03:13:32 +0200

    a

--- install.bat
+++ install.bat
@@ -1,25 +1,14 @@
 @echo off
 
-REM Function to install packages and show output
-:installPackages
-cd /d "%~dp0%~1"
-set "currentDir=%cd%"
-if exist node_modules (
-    echo %2 packages already installed in directory: "%currentDir%".
-) else (
-    echo Installing %2 packages in directory: "%currentDir%"...
-    npm i >nul 2>&1
-    if %errorlevel% neq 0 (
-        echo Failed to install %2 packages in directory: "%currentDir%".
-    ) else (
-        echo %2 packages installed successfully in directory: "%currentDir%".
-    )
-)
-echo.
+echo Installing npm packages for backend...
+cd backend
+npm install
+cd ..
 
-REM Install packages for backend and frontend
-call :installPackages "backend" Backend
-call :installPackages "frontend" Frontend
+echo Installing npm packages for frontend...
+cd frontend
+npm install
+cd ..
 
+echo Packages installation complete for both backend and frontend.
 pause
-exit
